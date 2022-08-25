eebe1a5a2352c06d7702afa4abe48cd39bd56276
cptShell <repozheka@gmail.com>
2022-08-25 16:26:37 +0300

    feat: add user creation test

--- src/routers/user.js
+++ src/routers/user.js
@@ -30,6 +30,7 @@ router.post('/users', async (req, res) => {
     sendWelcomeEmail(user.email, user.name);
     res.status(201).send({ user, token });
   } catch (error) {
+    console.log('error');
     res.status(400).send(error);
   }
 });
--- test/user.test.js
+++ test/user.test.js
@@ -1,1 +1,19 @@
 const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../src/app');
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+describe('User test', () => {
+  test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'shell',
+      email: 'supermail@gmail.com',
+      password: 'superpass123!',
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
