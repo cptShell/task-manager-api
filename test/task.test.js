88d7d689ede8cfbd47b58d3c3ef5932f96db7e7c
cptShell <repozheka@gmail.com>
2022-08-31 16:48:44 +0300

    feat: add more task testt

--- test/task.test.js
+++ test/task.test.js
@@ -6,12 +6,13 @@ const {
   setupDatabase,
   closeConnection,
   thirdTask,
+  firstTask,
 } = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 afterAll(closeConnection);
 
-describe('Task test', () => {
+describe('Positive task test', () => {
   test('Should create task for user', async () => {
     const response = await request(app)
       .post('/tasks')
@@ -44,5 +45,44 @@ describe('Task test', () => {
 
     const task = await Task.findById(thirdTask._id);
     expect(task).not.toBeNull();
+  });
+});
+
+describe('Negative task test', () => {
+  const invalidTasks = [
+    { description: [1], completed: true },
+    { description: 'New description', completed: 12345 },
+  ];
+
+  test('Should not create task with invalid description', async () => {
+    const response = await request(app)
+      .post('/tasks')
+      .set('Authorization', `Bearer ${user.tokens[0].token}`)
+      .send(invalidTasks[0])
+      .expect(400);
+  });
+
+  test('Should not create task with invalid completed value', async () => {
+    await request(app)
+      .post('/tasks')
+      .set('Authorization', `Bearer ${user.tokens[0].token}`)
+      .send(invalidTasks[1])
+      .expect(400);
+  });
+
+  test('Should not update task with invalid description', async () => {
+    await request(app)
+      .patch(`/tasks/${firstTask._id}`)
+      .set('Authorization', `Bearer ${user.tokens[0].token}`)
+      .send({ description: { name: 'Zhenya' } })
+      .expect(400);
+  });
+
+  test('Should not update task with invalid completed value', async () => {
+    await request(app)
+      .patch(`/tasks/${firstTask._id}`)
+      .set('Authorization', `Bearer ${user.tokens[0].token}`)
+      .send({ completed: 12345 })
+      .expect(400);
   });
 });
