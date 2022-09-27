0d534c313eef8e65a6d8d343340211397be62440
cptShell <repozheka@gmail.com>
2022-09-27 16:04:45 +0300

    fix: another cors try

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 require('./db/mongoose');
 const { userRouter, taskRouter } = require('./routers/routers');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
